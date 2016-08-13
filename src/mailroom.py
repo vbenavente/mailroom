4cfb241c08cbe6577772608510e9f1d5fd5164a5
Victor Benavente <vbenavente@hotmail.com>
2016-08-13 14:56:04 -0700

    fixed line too long linter errors

--- src/mailroom.py
+++ src/mailroom.py
@@ -8,18 +8,21 @@ DONORS = {
 
 
 def interface():
-    response = input(u"Press 1 to send a thank you note\nPress 2 to create a report ")
+    response = input(u"Press 1 to send a thank you note\n"
+                     u"Press 2 to create a report")
     while (response != '1') and (response != '2'):
         print(u"Please enter 1 or 2")
-        response = input(u"Press 1 to send a thank you note\nPress 2 to create a report ")
+        response = input(u"Press 1 to send a thank you note\n"
+                         u"Press 2 to create a report ")
     if response == '1':
-        name = input(u"Enter a donor\'s full name or enter the word 'list' to "
-                     u"see a list of donors ")
+        name = input(u"Enter a donor\'s full name or enter the "
+                     u"word 'list' to see a list of donors ")
         name = name.lower()
         while name == "list":
             for key in DONORS:
                 print(key)
-            name = input(u"Enter a donor\'s full name or enter the word 'list' to see a list of donors ")
+            name = input(u"Enter a donor\'s full name or enter the "
+                         u"word 'list' to see a list of donors ")
             name = name.lower()
         donation = input(u"Enter the donation amount for {0}".format(name))
         while not donation.isnumeric():
@@ -50,29 +53,12 @@ def compose_email(name, donation):
 
 
 def create_report():
-    # report_list = []
-    # for i in range(0, len(report_names(DONORS))):
-    #     person = []
-    #     names = report_names(DONORS)
-    #     totals = report_totals(DONORS)
-    #     num_donations = report_num_donations(DONORS)
-    #     average = report_average(report_totals(DONORS), report_num_donations(DONORS))
-    #     person.extend((names[i], totals[i], num_donations[i], average[i]))
-    #     report_list.append(person)
-
-    # report_list.sort(key=lambda x: x[1])
-    # report_list.reverse()
-    # print(report_list)
-    # interface()
-
     names = report_names(DONORS)
     totals = report_totals(DONORS)
     num_donations = report_num_donations(DONORS)
-    average = report_average(report_totals(DONORS), report_num_donations(DONORS))
-
+    average = report_average(report_totals(DONORS),
+                             report_num_donations(DONORS))
     report_list = zip(names, totals, num_donations, average)
-
-    # report_list.sort(key=lambda x: x[1])
     report_list = sorted(report_list, key=lambda x: x[1])
     report_list.reverse()
     print(report_list)

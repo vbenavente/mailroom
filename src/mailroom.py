c16afc660beb7f306518efde625a92192142c773
James <canning.jimmy@gmail.com>
2016-08-12 17:36:09 -0700

    Use zip to make a list of lists

--- src/mailroom.py
+++ src/mailroom.py
@@ -50,16 +50,30 @@ def compose_email(name, donation):
 
 
 def create_report():
-    report_list = []
-    for i in range(0, len(report_names(DONORS))):
-        person = []
-        names = report_names(DONORS)
-        totals = report_totals(DONORS)
-        num_donations = report_num_donations(DONORS)
-        average = report_average(report_totals(DONORS), report_num_donations(DONORS))
-        person.extend((names[i], totals[i], num_donations[i], average[i]))
-        report_list.append(person)
-    report_list.sort(key=lambda x: x[1])
+    # report_list = []
+    # for i in range(0, len(report_names(DONORS))):
+    #     person = []
+    #     names = report_names(DONORS)
+    #     totals = report_totals(DONORS)
+    #     num_donations = report_num_donations(DONORS)
+    #     average = report_average(report_totals(DONORS), report_num_donations(DONORS))
+    #     person.extend((names[i], totals[i], num_donations[i], average[i]))
+    #     report_list.append(person)
+
+    # report_list.sort(key=lambda x: x[1])
+    # report_list.reverse()
+    # print(report_list)
+    # interface()
+
+    names = report_names(DONORS)
+    totals = report_totals(DONORS)
+    num_donations = report_num_donations(DONORS)
+    average = report_average(report_totals(DONORS), report_num_donations(DONORS))
+
+    report_list = zip(names, totals, num_donations, average)
+
+    # report_list.sort(key=lambda x: x[1])
+    report_list = sorted(report_list, key=lambda x: x[1])
     report_list.reverse()
     print(report_list)
     interface()
@@ -96,5 +110,5 @@ def report_average(total_donated, num_donations):
     return report_average
 
 
-if __name__ == '__main__':
-    interface()
+# if __name__ == '__main__':
+#     interface()
